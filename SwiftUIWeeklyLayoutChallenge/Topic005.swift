809da9c6c00d81bc9e06e21c08c25848aa8d4315
uhooi <the_uhooi@yahoo.co.jp>
2022-08-10 21:16:13 +0900

    Impl Topic005View

--- SwiftUIWeeklyLayoutChallenge/Topic005.swift
+++ SwiftUIWeeklyLayoutChallenge/Topic005.swift
@@ -12,7 +12,50 @@ public struct Topic005View: View {
     public init() {}
     
     public var body: some View {
-        Text("Code your layout here!")
+        GroupBox {
+            ScrollView {
+                GroupBox {
+                    Text("- Stacksと`layoutPriority(_:)`を使ってみる")
+                } label: {
+                    Text("001")
+                }
+                GroupBox {
+                    Text(
+                        """
+                        - `NavigationLink`を使ってみる
+                        - `List`を使ってみる
+                        """
+                    )
+                } label: {
+                    Text("002")
+                }
+                GroupBox {
+                    Text(
+                        """
+                        - `Form`を使ってみる
+                        - `Picker`と`pickerStyle(_:)`を使ってみる
+                        - `fixedSize(horizontal:vertical:)`を使ってみる
+                        """
+                    )
+                } label: {
+                    Text("003")
+                }
+                GroupBox {
+                    Text("- `PrimitiveButtonStyle`を使ってみる")
+                } label: {
+                    Text("004")
+                }
+            }
+        } label: {
+            Group {
+                Text("#SwiftUIレイアウト一本勝負")
+                    .font(.system(.title2, design: .monospaced).weight(.heavy))
+                Text("主なテーマ")
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+        .fixedSize(horizontal: false, vertical: true)
+        .padding()
     }
 }
 
