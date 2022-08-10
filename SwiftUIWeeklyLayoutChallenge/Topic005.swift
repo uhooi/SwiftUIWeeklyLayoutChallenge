f28744c153aabc2b62a75bb54e17388bb080f2d6
uhooi <the_uhooi@yahoo.co.jp>
2022-08-10 20:25:48 +0900

    Add Topic005View

---
+++ SwiftUIWeeklyLayoutChallenge/Topic005.swift
@@ -0,0 +1,23 @@
+//
+//  Topic005.swift
+//  SwiftUIWeeklyLayoutChallenge
+//
+//  Created by treastrain on 2022/08/10.
+//
+
+import SwiftUI
+
+/// <doc:Topic005>
+public struct Topic005View: View {
+    public init() {}
+    
+    public var body: some View {
+        Text("Code your layout here!")
+    }
+}
+
+struct Topic005View_Previews: PreviewProvider {
+    static var previews: some View {
+        Topic005View()
+    }
+}
